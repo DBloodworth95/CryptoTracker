1903307950bc237c7ba34eef8cb5471c193f5c62
dan bloodworth <daniel.bloodworth18@my.northampton.ac.uk>
2020-06-24 11:11:13 +0100

    Refactor

--- src/main/java/cryptotracker/com/controller/MainController.java
+++ src/main/java/cryptotracker/com/controller/MainController.java
@@ -7,23 +7,13 @@ import javafx.animation.FillTransition;
 import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
 
 public class MainController {
-
-    private static final double WIDTH = 800;
-
-    private static final double HEIGHT = 800;
-
-    @FXML
-    private AnchorPane mainPane;
     @FXML
     private Rectangle backgroundRect;
-    @FXML
-    private Label btcLabel;
     @FXML
     private Label btcPriceLabel;
 
